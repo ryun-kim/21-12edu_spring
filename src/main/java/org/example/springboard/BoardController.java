2ccda0744ae6032a3357650d69aaa67279afb7e3
ryun <kjr6260@naver.com>
2021-12-28 10:44:46 +0900

    21-12-28 스프링

---
+++ src/main/java/org/example/springboard/BoardController.java
@@ -0,0 +1,11 @@
+package org.example.springboard;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BoardController {
+
+    @RequestMapping("/list")
+    public void list() {}
+}
