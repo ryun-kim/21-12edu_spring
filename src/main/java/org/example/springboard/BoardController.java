ef5084a68bdfe506ae1d99081b6f1eb2e98d2b42
ryun <kjr6260@naver.com>
2021-12-28 16:42:29 +0900

    12-28 스프링 세팅완료! , insert문 , select list 완성

--- src/main/java/org/example/springboard/BoardController.java
+++ src/main/java/org/example/springboard/BoardController.java
@@ -1,11 +1,37 @@
 package org.example.springboard;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
-public class BoardController {
+import java.util.List;
 
-    @RequestMapping("/list")
-    public void list() {}
+@Controller
+@RequestMapping("/board")
+public class BoardController {
+    @Autowired
+    private BoardService service;
+
+
+
+    @GetMapping("/list")
+    public void list(Model model){
+        List<BoardEntity> list = service.selBoardList();
+        model.addAttribute("list",list);
+    }
+
+    @GetMapping("/write")
+    public void write(){}
+
+    @PostMapping("/write")
+    public String writeProc(BoardEntity entity){
+        System.out.println(entity);
+        int result = service.insBoard(entity);
+        return "redirect:/board/list";
+    }
+
+
 }
---
+++ src/main/java/org/example/springboard/BoardService.java
@@ -0,0 +1,20 @@
+package org.example.springboard;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BoardService {
+    @Autowired
+    private BoardMapper mapper;
+
+    public int insBoard(BoardEntity entity){
+        return mapper.insBoard(entity);
+    }
+
+    public List<BoardEntity> selBoardList(){
+        return mapper.selBoardList();
+    }
+}
